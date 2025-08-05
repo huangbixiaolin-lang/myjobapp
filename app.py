ad70720c51ae23acc5fb82b51ba9ac050bbe93de
huangbixiaolin-lang <huangbixiaolin@gmail.com>
2025-08-05 23:33:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -109,4 +109,6 @@ def history():
     return render_template("history.html", records=records)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
